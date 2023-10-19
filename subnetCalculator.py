134cc4509f5950739a1e18b3b841a3ff4332ef73
thaminultazbir <thaminultazbir1999@gmail.com>
2023-10-19 16:37:06 +0600

    ip validitity check

---
+++ subnetCalculator.py
@@ -0,0 +1,16 @@
+import random
+import sys
+
+def subnetCalculator():
+    try:
+        print("\n")
+
+        while True:
+            ip_address = input("Input IP address: ")
+            ip_octets = ip_address.split(".")
+
+            if (len(ip_octets) == 4) and (1 <= int(ip_octets[0]) <= 223) and (int(ip_octets[0]) != 127) and (int(ip_octets[0]) != 169 or int(ip_octets[1]) != 254) and (0 <= int(ip_octets[1]) <= 255 and 0 <= int(ip_octets[2]) <= 255 and 0 <= int(ip_octets[3]) <= 255):
+                break
+
+            else:
+                print("Invalid IP address! Please retry")
