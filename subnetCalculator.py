1a3fe3b10be09b74594124a2517317bb2b4c1536
thaminultazbir <thaminultazbir1999@gmail.com>
2023-10-22 10:29:09 +0600

    Added masking

--- subnetCalculator.py
+++ subnetCalculator.py
@@ -14,3 +14,8 @@ def subnetCalculator():
 
             else:
                 print("Invalid IP address! Please retry")
+                continue
+        masks = [255, 254, 252, 248, 240, 224, 192, 128, 0]
+
+    except:
+        print('')
